0103a4dc37cd90a5795e1a8bb97ddb4e4732dae7
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2020-09-25 09:54:39 +0530

    made tell tell_kuki

--- kuki.py
+++ kuki.py
@@ -5,19 +5,20 @@ import sys
 import string
 import urllib.parse
 
-import bs4
 import requests
 import os
 import discord
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path = "token.env")
-token = os.getenv("DISCORD_TOKEN")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 client = discord.Client()
-
-url = 'https://www.pandorabots.com/mitsuku/'
-session = requests.Session()
 
 @client.event
 async def on_ready():
@@ -25,35 +26,41 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-        if not message.author == client.user:
-            await message.channel.send(tell(message.content))
+    if str(message.channel)=='chat-with-kuki' and not (message.author == client.user  or str(message.author)=='k4u5h1k#8653'):
+        response = tell_kuki(message.content)
+        response = response.replace('Steve Worswick','Kaushik Sivashankar')
+        await message.channel.send(response)
+
 
 def new_client():
-    client_name = 'kukilp-'+''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
+    try:
+        driver.get("https://pandorabots.com/mitsuku/")
+
+        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "pb-widget__description__chat-now__button"))).click()
+        wait.until(EC.element_to_be_clickable((By.ID, "pb-widget-input-field"))).send_keys("Hey"+Keys.ENTER)
+
+        browser_log = str(driver.get_log('performance'))
+        client_name = re.search(r'kukilp-.{10}',browser_log)[0]
+        driver.quit()
+
+    except:
+        driver.quit()
+        print("Could not forge client, making a random one instead")
+        client_name = 'kukilp-'+''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
+
     return client_name
-
-# def new_botkey():
-#     main_page = session.get(url).text
-#     main_soup = bs4.BeautifulSoup(main_page, 'lxml')
-#     botkey = re.search(r'PB_BOTKEY: "(.*)"', main_page).groups()[0]
-#     return botkey
-
 
 def postit(botkey, query, client_name, sessionid):
     data = f'input={query}&botkey={botkey}&client_name={client_name}&sessionid={sessionid}&channel=6'
     return session.post('https://miapi.pandorabots.com/talk', data = data).text
 
-client_name = new_client()
-botkey = "n0M6dW2XZacnOgCWTp0FRYUuMjSfCkJGgobNpgPv9060_72eKnu3Yl-o1v2nFGtSXqfwJBG2Ros~"
-sessionid = "null"
 
-fail_count = 0
-
-def tell(query):
+def tell_kuki(query):
     global botkey, sessionid, client_name, channel, fail_count
 
     if fail_count > 2:
-        return "Sorry Error Occured"
+        fail_count = 0
+        return "An Error occured, because Cosec cannot code, restarting..."
 
     session.headers.update({
         'User-Agent': ' '.join(('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2)',
@@ -73,7 +80,7 @@ def tell(query):
     except Exception as e:
         print("Error", e)
         fail_count+=1
-        return tell(query)
+        return tell_kuki(query)
 
     response_json = json.loads(response_raw)
     sessionid = response_json.get('sessionid')
@@ -82,11 +89,32 @@ def tell(query):
 
     if len(response)==0:
         print("zero response")
+        print(response_json)
         client_name = new_client()
         fail_count+=1
-        return tell(query)
+        return tell_kuki(query)
 
     fail_count = 0
+
     return response
 
-client.run(token)
+if __name__ == "__main__":
+    caps = DesiredCapabilities.CHROME
+    caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options = chrome_options, desired_capabilities=caps)
+    wait = WebDriverWait(driver, 20)
+
+    url = 'https://www.pandorabots.com/mitsuku/'
+    session = requests.Session()
+    client_name = new_client()
+    botkey = "n0M6dW2XZacnOgCWTp0FRYUuMjSfCkJGgobNpgPv9060_72eKnu3Yl-o1v2nFGtSXqfwJBG2Ros~"
+    sessionid = "null"
+
+    fail_count = 0
+
+    load_dotenv(dotenv_path = "token.env")
+    token = os.getenv("DISCORD_TOKEN")
+    client.run(token)
