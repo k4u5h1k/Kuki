db583bba2cd1a8a352150820cb0775c63ce6d279
Ubuntu <kaushik@pentest.uj4camrgcgiubbt0jwxfdf4tfc.bx.internal.cloudapp.net>
2020-10-02 09:32:02 +0000

    v4

--- kuki.py
+++ kuki.py
@@ -10,6 +10,9 @@ import os
 import discord
 from dotenv import load_dotenv
 
+from googletrans import Translator
+from indictrans import Transliterator
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -19,6 +22,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 client = discord.Client()
+kaushik = re.compile('Kaushik( Sivashankar)?',re.IGNORECASE)
+stevey = re.compile('Steve( Worswick)?',re.IGNORECASE)
+translator = Translator()
 
 
 @client.event
@@ -28,15 +34,32 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if str(message.channel) == 'chat-with-kuki' and not message.author == client.user:
-        response = tell_kuki(message.content)
-        response = response.replace('Steve Worswick', 'Kaushik Sivashankar')
-        await message.channel.send(response)
+    if str(message.channel) == 'chat-with-kuki' and message.author != client.user:
+        if message.content[0] != '/':
+            if '>' in message.content:
+                await message.channel.send('I do not support quoted messages and users for now, sorry!')
+
+            else:
+                translateResponse = False
+
+                if translator.detect(message.content).lang == 'hi':
+                    message.content = translator.translate(message.content).text
+                    translateResponse = True
+
+                new_message = kaushik.sub('Steve Worswick', message.content)
+                response = stevey.sub('Kaushik',tell_kuki(new_message))
+
+                if translateResponse:
+                    response = translator.translate(response,src='en',dest='hi').text
+                    trn = Transliterator(source='hin', target='eng', build_lookup=True)
+                    response = trn.transform(response).replace('kowshik','Kaushik')
+
+                await message.channel.send(response)
 
 
 def new_client():
     try:
-        driver.get("https://pandorabots.com/mitsuku/")
+        driver.get(url)
 
         wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "pb-widget__description__chat-now__button"))).click()
         wait.until(EC.element_to_be_clickable((By.ID, "pb-widget-input-field"))).send_keys("Hey"+Keys.ENTER)
@@ -54,8 +77,8 @@ def new_client():
 
 
 def postit(botkey, query, client_name, sessionid):
-    data = f'input={query}&botkey={botkey}&client_name={client_name}&sessionid={sessionid}&channel=6'
-    return session.post('https://miapi.pandorabots.com/talk', data=data).text
+    data = f'input={query}&sessionid={sessionid}&channel=6&botkey={botkey}&client_name={client_name}'
+    return requests.post('https://miapi.pandorabots.com/talk', data=data, headers=headers).text
 
 
 def tell_kuki(query):
@@ -65,13 +88,6 @@ def tell_kuki(query):
         fail_count = 0
         return "An Error occured, because Kaushik sucks, restarting..."
 
-    session.headers.update({
-        'User-Agent': ' '.join(('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2)',
-                                'AppleWebKit/537.36 (KHTML, like Gecko)',
-                                'Chrome/72.0.3626.119 Safari/537.36')),
-        'Referer': url,
-        'Content-type': 'application/x-www-form-urlencoded'
-    })
 
     tosend = re.sub(r'\s+', ' ', query).strip()
     tosend = urllib.parse.quote(tosend)
@@ -87,19 +103,13 @@ def tell_kuki(query):
 
     response_json = json.loads(response_raw)
     sessionid = response_json.get('sessionid')
-    response = list(re.sub(r'<.*?>', ' ', i) for i in response_json['responses'][0].split("\u200e"))
-    response = '\n'.join(response)
-
-    if len(response) == 0:
-        print("zero response")
-        print(response_json)
-        client_name = new_client()
-        fail_count+=1
-        return tell_kuki(query)
+    # response = list(re.sub(r'<.*?>', ' ', i) for i in response_json['responses'][0].split("\u200e"))
+    response = '\n'.join(response_json['responses'][0].split("\u200e"))
 
     fail_count = 0
 
     return response
+
 
 if __name__ == "__main__":
     caps = DesiredCapabilities.CHROME
@@ -111,7 +121,17 @@ if __name__ == "__main__":
     wait = WebDriverWait(driver, 20)
 
     url = 'https://www.pandorabots.com/mitsuku/'
-    session = requests.Session()
+
+    headers = {
+        'host': 'miapi.pandorabots.com',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:81.0) Gecko/20100101 Firefox/81.0',
+        'Accept':'*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Referer': url,
+        'Content-type': 'application/x-www-form-urlencoded'
+    }
+
     client_name = new_client()
     botkey = "n0M6dW2XZacnOgCWTp0FRYUuMjSfCkJGgobNpgPv9060_72eKnu3Yl-o1v2nFGtSXqfwJBG2Ros~"
     sessionid = "null"
